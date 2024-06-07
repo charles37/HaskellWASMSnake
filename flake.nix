ab0e5e04051f49c03f8a9c89deaaf2a3562ea6c9
charles37 <charles37@github.com>
2024-06-07 11:47:47 -0400

    update

--- flake.nix
+++ flake.nix
@@ -3,41 +3,45 @@
     flake-utils.url = "github:numtide/flake-utils";
     ghc-wasm-meta.url = "gitlab:ghc/ghc-wasm-meta?host=gitlab.haskell.org";
   };
-  outputs = { self, nixpkgs, flake-utils, ghc-wasm-meta }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ghc-wasm-meta,
+  }:
     flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = (import nixpkgs { inherit system; }).pkgs;
-        in
-        {
-          packages = flake-utils.lib.flattenTree {
-            update = pkgs.writeShellApplication {
-              name = "update";
-              runtimeInputs = [
-                ghc-wasm-meta.packages.${system}.default
-              ];
-              text = ''
-                wasm32-wasi-cabal update
-              '';
-            };
-            build = pkgs.writeShellApplication {
-              name = "build";
-              runtimeInputs = [
-                pkgs.nodejs
-                ghc-wasm-meta.packages.${system}.default
-                # pkgs.busybox
-              ];
-              text = ''
-                npm install
-                npm run build
-                wasm32-wasi-cabal build
-                HaskellWASMSnake=$(wasm32-wasi-cabal list-bin exe:HaskellWASMSnake)
-                cp "$HaskellWASMSnake" dist
-              '';
-            };
+    (
+      system: let
+        inherit ((import nixpkgs {inherit system;})) pkgs;
+      in {
+        packages = flake-utils.lib.flattenTree {
+          update = pkgs.writeShellApplication {
+            name = "update";
+            runtimeInputs = [
+              ghc-wasm-meta.packages.${system}.default
+            ];
+            text = ''
+              wasm32-wasi-cabal update
+            '';
           };
-        }
-      );
+          build = pkgs.writeShellApplication {
+            name = "build";
+            runtimeInputs = [
+              pkgs.nodejs
+              ghc-wasm-meta.packages.${system}.default
+              # pkgs.busybox
+            ];
+            text = ''
+              npm install
+              npm run build
+              wasm32-wasi-cabal build
+              HaskellWASMSnake=$(wasm32-wasi-cabal list-bin exe:HaskellWASMSnake)
+              cp "$HaskellWASMSnake" dist
+            '';
+          };
+        };
+      }
+    );
 }
-
 # --with-gcc ${pkgs.gcc}/bin/gcc
+
